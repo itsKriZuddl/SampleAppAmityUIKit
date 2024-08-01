602b422b8ad1684086b2dd508585ec23d9e907f5
krishna101 <krishna@zuddl.com>
2024-08-01 09:06:55 +0530

    updates

--- SampleUIAmityPro/SampleUIAmityPro/AppDelegate.swift
+++ SampleUIAmityPro/SampleUIAmityPro/AppDelegate.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
+import AmitySDK
 import AmityUIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Setup AmityUIKit
+        AppManager.shared.setupAmityUIKit()
+
+        if #available(iOS 13.0, *) {
+            // on newer 13.0 version, the window setup finished on `SceneDelegate`
+        } else {
+            window = UIWindow()
+            window?.rootViewController = AppManager.shared.startingPage()
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
@@ -32,7 +46,81 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        // Handler of opening external url from web browsing session.
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+
+            let urlString = url.absoluteString //"https://Amity.co/post/124325135"
+            // Parse url and be sure that it is a url of a post
+            if urlString.contains("post/") {
+                if let range = urlString.range(of: "post/") {
+                    // Detect id of the post
+                    let postId = String(urlString[range.upperBound...])
+                    
+                    // Open post details page
+                    openPost(withId: postId)
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    // MARK: Push Notification
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // https://developer.apple.com/documentation/usernotifications/registering_your_app_with_apns
+        // Forward Tokens to Your Provider Server
+        AppManager.shared.registerDeviceToken(deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Fail didFailToRegisterForRemoteNotificationsWithError: \(error.localizedDescription)")
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 0  // reset badge counter
+    }
+
 
 }
 
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // For this sample app, we allow every push notification, to present while the app is in foreground.
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+    }
+    
+}
+
+// MARK:- Helper methods
+extension AppDelegate {
+    
+    private func openPost(withId postId: String) {
+        window = UIWindow()
+        AmityUIKitManager.registerDevice(withUserId: "victimIOS", displayName: "victimIOS".uppercased(), sessionHandler: SampleSessionHandler())
+        
+        let postDetailViewController = AmityPostDetailViewController.make(withPostId: "c1bb8697c88a01f6423765984a3e47ac")
+        window?.rootViewController = postDetailViewController
+        window?.makeKeyAndVisible()
+    }
+    
+}
+
+
+class SampleSessionHandler: SessionHandler {
+    
+    func sessionWillRenewAccessToken(renewal: AccessTokenRenewal) {
+        renewal.renew()
+    }
+    
+}
 
--- SampleUIAmityPro/SampleUIAmityPro/SceneDelegate.swift
+++ SampleUIAmityPro/SampleUIAmityPro/SceneDelegate.swift
@@ -19,24 +19,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 //        guard let _ = (scene as? UIWindowScene) else { return }
         
+//        if let windowScene = scene as? UIWindowScene {
+//            
+//            //             let client = try? AmityClient(apiKey: "b0eab9596e88a2364461844f010e4288825f85b3ef323b2c", region: .SG)
+//
+//            
+//            // Add the API key and region logic from the main application
+//            AmityUIKitManager.setup(apiKey: "b0eaec5d398ef36c4f62851c53001489d55a8ab6bf366a2c", region: .US)
+//            
+//            AmityUIKitManager.set(theme: AmityTheme.init(primary: UIColor.red, base: UIColor.green))
+//            
+//            let viewController = AmityCommunityHomePageViewController.make()
+//
+//            // push
+//    //        navigationController?.pushViewController(viewController, animated: true)
+//
+//            // present
+//            let navigationController = UINavigationController(rootViewController: viewController)
+////            present(navigationController, animated: true, completion: nil)
+//            
+//                let window = UIWindow(windowScene: windowScene)
+//                window.rootViewController =  navigationController
+//                self.window = window
+//                window.makeKeyAndVisible()
+//            }
+        
         if let windowScene = scene as? UIWindowScene {
-            // Add the API key and region logic from the main application
-            AmityUIKitManager.setup(apiKey: "b0eab9596e88a2364461844f010e4288825f85b3ef323b2c", region: .SG)
-            
-            let viewController = AmityCommunityCreatorViewController.make()
-
-    //        // push
-    //        navigationController?.pushViewController(viewController, animated: true)
-
-            // present
-            let navigationController = UINavigationController(rootViewController: viewController)
-//            present(navigationController, animated: true, completion: nil)
-            
-                let window = UIWindow(windowScene: windowScene)
-                window.rootViewController =  navigationController
-                self.window = window
-                window.makeKeyAndVisible()
-            }
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = AppManager.shared.startingPage()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -66,7 +79,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        
+        // Handler of opening external url from web browsing session.
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+            // Parse url and be sure that it is a url of a post
+            let urlString = url.absoluteString //"https://Amity.co/post/124325135"
+            if urlString.contains("post/") {
+                if let range = urlString.range(of: "post/") {
+                    // Detect id of the post
+                    let postId = String(urlString[range.upperBound...])
+                    
+                    // Open post details page
+                    openPost(withId: postId, scene: scene)
+                }
+            }
+        }
+    }
 
 
 }
 
+@available(iOS 13.0, *)
+extension SceneDelegate {
+    func openPost(withId postId: String, scene: UIScene) {
+        guard let _ = (scene as? UIWindowScene), let windowScene = scene as? UIWindowScene else { return }
+        AmityUIKitManager.registerDevice(withUserId: "victimIOS", displayName: "victimIOS".uppercased(), sessionHandler: SampleSessionHandler())
+        let window = UIWindow(windowScene: windowScene)
+        let postDetailViewController = AmityPostDetailViewController.make(withPostId: "c1bb8697c88a01f6423765984a3e47ac")
+            
+        window.rootViewController = postDetailViewController
+        self.window = window
+        window.makeKeyAndVisible()
+        
+    }
+}
